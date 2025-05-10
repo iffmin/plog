583e078a76d5c7087d044671f060d6cb234b163b
woomin <thddnals1@naver.com>
2025-05-10 19:42:14 +0900

    planner modify

---
+++ src/main/java/com/plog/PLOG/controller/controller2.java
@@ -0,0 +1,30 @@
+package com.plog.PLOG.controller;
+
+import com.plog.PLOG.entity.Place;
+import com.plog.PLOG.service.MyPlaceService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class controller2 {
+    @GetMapping("/planner")
+    public String planner(){
+        return "planner";
+    }
+
+}
+
+@RestController
+@RequestMapping("/Place")
+class PlaceRestController {
+
+    @Autowired
+    private MyPlaceService placeService;
+
+    @PostMapping("/save")
+    public Place savePlace(@RequestBody Place Place) {
+        return placeService.savePlace(Place);
+    }
+}
